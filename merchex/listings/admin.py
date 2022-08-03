0a2eb555a366370daa6b189a84854e7dc961e7fb
Nicolas HOARAU <nicolas.hoarau@odyssey.re>
2022-08-03 16:30:53 +0400

    Django 0.2

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from listings.models import Band
+
+class BandAdmin(admin.ModelAdmin):
+	list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+
+admin.site.register(Band, BandAdmin)
+
+from listings.models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+	list_display = ('title', 'description', 'sold','band')
+
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,11 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+	# Create your models here.
 
-#listings/models.py
+	#listings/models.py
 
 class Band(models.Model):
+
+	def __str__(self):
+		return f'{self.name}'
+	class Genre(models.TextChoices):
+			HIP_HOP = 'HH'
+			SYNTH_POP = 'SP'
+			ALTERNATIVE_ROCK = 'AR'
+
 	name = models.fields.CharField(max_length=100)
+	genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+	biography = models.fields.CharField(max_length=1000)
+	year_formed = models.fields.IntegerField(
+	validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+	)
+	active = models.fields.BooleanField(default=True)
+	official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+	class TypeC(models.TextChoices):
+			RECORDS = 'disques'
+			CLOTHING = 'vetements'
+			POSTERS = 'affiches'
+			MISCELLANEOUS ='divers'
+
 	title = models.fields.CharField(max_length=100)
+	description = models.fields.CharField(max_length=100)
+	sold = models.fields.BooleanField(default=False)
+	year = models.fields.IntegerField(null=True, blank=True)
+	type = models.fields.CharField(choices=TypeC.choices, max_length=20)
+
+	band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
